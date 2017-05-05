20de4a1d7243e4a4c2932c19037b24823763731d
ncomptier <ncomptier@e-biz.fr>
2017-05-05 15:49:00 +0200

    [Feat] Routing and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { AppComponent }        from './app.component';
+import { AppRoutingModule }   from './app-routing.module';
+import { AppComponent} from './app.component';
+import { HeroesComponent }        from './heroes.component';
+import { DashboardComponent }         from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }         from './hero.service';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
